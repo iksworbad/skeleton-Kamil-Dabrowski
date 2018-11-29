50b3c1b841447cdd631cb2a658eca80bf4a8f790
Kamil Dąbrowski <kamildabrowski@iMac-Kamil.local>
2018-11-29 02:48:30 +0100

    final project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const concatCss = require('gulp-concat-css');
+const cleanCss = require('gulp-clean-css');
+const uncss = require('gulp-uncss')
 
-gulp.task('server', function(){
+
+gulp.task('server',['css'], function(){
     browserSync.init({
         server: "./"
     });
-    gulp.watch(["./*.html", "./css/*.css"]).on('change',browserSync.reload);
-})
+    gulp.watch(["./*.html", "./css/*.css", ['css']]).on('change',browserSync.reload);
+});
+
+gulp.task('css', ()=>{
+    return gulp.src(['./css/skeleton.css','./css/normalize.css','./css/main.css'])
+        .pipe(concatCss("bundle.css"))
+        .pipe(uncss({html: ['index.html']}))
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('css'));
+});
 
 gulp.task('default', ['server']);
